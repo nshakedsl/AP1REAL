01d2a84578348a14c3bf7e5a9ecd0008d1ce09b6
OfirGurvits <ofirgurvits@gmail.com>
2022-11-17 10:54:00 +0200

    added proper printing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 #include "Distances.h"
+#define type long double
 using namespace std;
 //parses a string into a vector of ints, needs to get a string composed of numbers seperated by spaces
 //todo: make sure works with empty string
+void printLongDouble(type value, bool val = true){
+    if(value-(int)value == 0)
+        cout.precision(1);
+    else
+        cout.precision(16);
+    cout << value;
+    if (val)
+        cout << endl;
+}
+
 vector<long double> parseString(const string& s){
     int counter = 0;
     int prev = 0;
@@ -26,9 +36,9 @@ int main() {
     string input1, input2;
     // receives 2 vectors from the user
     getline(cin,input1, '\n');
-    vector<long double> v1 = parseString(input1);
+    vector<type> v1 = parseString(input1);
     getline(cin,input2, '\n');
-    vector<long double> v2 = parseString(input2);
+    vector<type> v2 = parseString(input2);
     if(v1.empty()){
         cout<<"empty vector, bye!"<<endl;
         return 0;
@@ -39,11 +49,11 @@ int main() {
     }
     Distances d= Distances(v1, v2);
     //calculate all distances and print them
-    cout  << setprecision(16);
-    cout << d.euclidianDistance() << endl;
-    cout << d.manhattenDistance() << endl;
-    cout << d.chebyshevDistance() << endl;
-    cout << d.canberraDistance() << endl;
-    cout << d.minkowskiDistance();
+    fixed(cout);
+    printLongDouble(d.euclidianDistance());
+    printLongDouble(d.manhattenDistance());
+    printLongDouble(d.chebyshevDistance());
+    printLongDouble(d.canberraDistance());
+    printLongDouble(d.minkowskiDistance(), false);
     return 0;
 }
