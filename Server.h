10ce192e6e9dd8687674ae8c7d3117ae6521e48b
nshakedsl <shakedsolomon@gmail.com>
2023-01-01 18:13:41 +0200

    create server files

---
+++ Server.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/socket.h>
+#include <bits/socket_type.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+class Server {
+
+};
+
+
+#endif //AP1_SERVER_H
