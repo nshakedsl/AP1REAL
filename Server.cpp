8ddf5bec1ed59c7d4f7dac020c29ae474ad834bb
OfirGurvits <ofirgurvits@gmail.com>
2023-01-02 20:46:50 +0200

    improved documentation

--- Server.cpp
+++ Server.cpp
@@ -2,7 +2,6 @@
 #include <bits/socket.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <iostream>
 #include <unistd.h>
 #include <cstring>
 #include "Server.h"
@@ -12,37 +11,41 @@ Server::Server(int port,std::string file) {
     this->file = file;
 }
 
+//serves a given client properly
 void Server::serve(int client_sock) {
     char buffer[4096];
-    int expected_data_len = sizeof(buffer);
-    long read_bytes = recv(client_sock, buffer, expected_data_len, 0);
-    //empty message signals close connection
-    if (read_bytes == 0) {
-        close(client_sock);
-        return;
-    //negative bytes read indicate an error has occured
-    } else if (read_bytes < 0) {
-        perror("error reading bytes to client");
-        close(client_sock);
-        return;
-    }
-    //everything was fine, check client message
-    Parser parser = Parser(buffer);
-    if(parser.validInput()){
-        //todo: fix me
+    //
+    while (true) {
+        int expected_data_len = sizeof(buffer);
+        long read_bytes = recv(client_sock, buffer, expected_data_len, 0);
+        //empty message signals close connection
+        if (read_bytes == 0) {
+            close(client_sock);
+            return;
+        //negative bytes read indicate an error has occurred
+        } else if (read_bytes < 0) {
+            perror("error reading bytes to client");
+            close(client_sock);
+            return;
+        }
+        //everything was fine, check client message
+        Parser parser = Parser(buffer);
+        if (parser.validInput()) {
+            //todo: fix me
         //Classification classification = Classification();
-    } else{
-        strcpy(buffer,"invalid input");
+        } else {
+            strcpy(buffer, "invalid input");
+        }
+        unsigned long bytes_sent = strlen(buffer);
+        long sent_bytes = send(client_sock, buffer, bytes_sent, 0);
+        if (sent_bytes < 0) {
+            perror("error sending to client");
+        }
     }
-    unsigned long bytes_sent = strlen(buffer);
-    long sent_bytes = send(client_sock, buffer, bytes_sent, 0);
-    if (sent_bytes < 0) {
-        perror("error sending to client");
-    }
-    close(client_sock);
 }
 
 void Server::run() {
+    //creates the server socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("error creating socket");
@@ -53,23 +56,27 @@ void Server::run() {
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = INADDR_ANY;
     sin.sin_port = htons(port);
+    //binds the socket to the given port
     if (bind(sock, (struct sockaddr *) &sin, sizeof(sin)) < 0) {
         perror("error binding socket");
         exit(1);
     }
-
+    //runs forever, continuously serves clients one after another
     while (true){
+        //tries to accept a socket
         if (listen(sock, 5) < 0) {
             perror("error listening to a socket");
             exit(1);
         }
         struct sockaddr_in client_sin;
         unsigned int addr_len = sizeof(client_sin);
+        //accepts a socket
         int client_sock = accept(sock, (struct sockaddr *) &client_sin, &addr_len);
         if (client_sock < 0) {
             perror("error accepting client");
             exit(1);
         }
+        //serves said socket
         serve(client_sock);
     }
 
