6e682ebea82c3347bb1fb32b3460f9e2ebae081f
nshakedsl <shakedsolomon@gmail.com>
2023-01-03 16:08:01 +0200

    fix bug vectors not in the same length

--- VectorCollection.cpp
+++ VectorCollection.cpp
@@ -9,14 +9,14 @@ void VectorCollection::pushVector(const std::vector<double> &vec) {
     vectorList.push_back(vec);
 }
 
-std::vector<vector<double>>
+std::vector <vector<double>>
 VectorCollection::getDistancesK(const std::vector<double> &vector, const std::string &format, int k) {
     //initializes a sorted multimap
     std::multimap<double, std::vector<double>> sortedVecs = getDistances(vector, format);
     //an empty vector of vectors
-    std::vector<std::vector<double>> kVectors;
+    std::vector <std::vector<double>> kVectors;
     int counter = 0;
-    if (k > vectorList.size())
+    if (k > vectorList.size() || sortedVecs.size() == 0)
         return kVectors;
     //adds the first k vectors in the map
     for (auto &sortedVec: sortedVecs) {
@@ -41,24 +41,29 @@ VectorCollection::getDistances(const std::vector<double> &vector, const std::str
     //iterates over all the vectors
     for (std::vector<double> v: vectorList) {
         //calculate the distance of the current vector with the given one
-        Distances distance = Distances(v, vector);
-        if (format == "AUC")
-            dis = distance.euclidianDistance();
-        else if (format == "MAN")
-            dis = distance.manhattenDistance();
-        else if (format == "CHB")
-            dis = distance.chebyshevDistance();
-        else if (format == "CAN")
-            dis = distance.canberraDistance();
-        else if (format == "MIN")
-            dis = distance.minkowskiDistance();
-            //illegal type of distance given
-        else {
-            std::cout << "error" << endl;
-            exit(1);
+        try {
+            Distances distance = Distances(v, vector);
+            if (format == "AUC")
+                dis = distance.euclidianDistance();
+            else if (format == "MAN")
+                dis = distance.manhattenDistance();
+            else if (format == "CHB")
+                dis = distance.chebyshevDistance();
+            else if (format == "CAN")
+                dis = distance.canberraDistance();
+            else if (format == "MIN")
+                dis = distance.minkowskiDistance();
+                //illegal type of distance given
+            else {
+                std::cout << "error" << endl;
+                exit(1);
+            }
+            //inserts the distance of v from the current vector
+            vectors.insert(pair < double, std::vector < double >> (dis, v));
+        } catch (...) {
+            return vectors;
         }
-        //inserts the distance of v from the current vector
-        vectors.insert(pair<double, std::vector<double>>(dis, v));
+
     }
     //returns the list of distance, vector
     return vectors;
