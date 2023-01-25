08b847873aab79665437311fe2877a99b41f01e6
OfirGurvits <ofirgurvits@gmail.com>
2023-01-25 17:30:16 +0200

    trial socket

--- SocketIO.h
+++ SocketIO.h
@@ -2,10 +2,13 @@
 #define AP1REAL_SOCKETIO_H
 #include "DefaultIO.h"
 #define SIZE 10
+#define BUFFER_SIZE 4096
 #include <cstring>
 class SocketIO : public DefaultIO{
 private:
     int sock;
+    std::string readP();
+    void writeP(const std::string &x);
 public:
     std::string read() override;
     void write(const std::string &x) override;
