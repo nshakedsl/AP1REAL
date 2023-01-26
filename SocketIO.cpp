0fbd3e939022f2f1944ae5abb9f0b1db84551d8d
OfirGurvits <ofirgurvits@gmail.com>
2023-01-26 12:00:18 +0200

    new sock

--- SocketIO.cpp
+++ SocketIO.cpp
@@ -2,17 +2,19 @@
 #include <stdexcept>
 #include <sstream>
 #include "SocketIO.h"
+
 SocketIO::SocketIO(int sock) {
     this->sock = sock;
 }
 
-bool isValidInteger(const std::string& str) {
+bool isValidInteger(const std::string &str) {
     std::istringstream ss(str);
     int x;
     ss >> x;
     return !ss.fail() && ss.eof();
 }
 
+/*
 std::string SocketIO::read() {
     char bufferSize[SIZE + 1] = {0}; // for the message
     int read_bytes = recv(sock, bufferSize, SIZE, 0);
@@ -64,5 +66,50 @@ void SocketIO::write(const std::string &x) {
     {
         throw std::runtime_error("error sending to socket");
     }
+}
+ */
+void SocketIO::write(const std::string &y) {
+    std::string x = y;
+    //ADD ZEROS:
+    x += DELIM;
+    char buffer[CONST + 1];
+    size_t len = x.length();
+    int index = 0;
+    while (len > CONST) {
+        len -= CONST;
+        strcpy(buffer, x.substr(index, CONST).c_str());
+        index += CONST;
+        ssize_t sent_bytes = send(sock, buffer, CONST+1, 0);
+        if (sent_bytes < 0) {
+            throw std::runtime_error("error sending to socket");
+        }
+    }
+    ssize_t sent_bytes = send(sock, x.substr(index).c_str(), strlen(x.substr(index).c_str()) + 1, 0);
+    if (sent_bytes < 0) {
+        throw std::runtime_error("error sending to socket");
+    }
 
+}
+
+std::string SocketIO::read() {
+    std::string ret;
+    char buffer[CONST + 1] = {0}; // for the message
+    char *found;
+    ssize_t read_bytes;
+    do {
+        read_bytes = recv(sock, buffer, CONST+1, 0);
+        found = std::find(std::begin(buffer), std::end(buffer), DELIM);
+        if (found == std::end(buffer))
+            ret += buffer;
+        else{
+            buffer[strlen(buffer)-1] = '\0';
+            ret += buffer;
+        }
+        if (read_bytes == 0) {
+            throw std::runtime_error("connection closed");
+        } else if (read_bytes < 0) {
+            throw std::runtime_error("error");
+        }
+    } while (found == std::end(buffer));
+    return ret;
 }
--- SocketIO.h
+++ SocketIO.h
@@ -3,12 +3,15 @@
 #include "DefaultIO.h"
 #define SIZE 10
 #include <cstring>
+#include "algorithm"
+#define CONST 4096
+#define DELIM '%'
 class SocketIO : public DefaultIO{
 private:
     int sock;
 public:
     std::string read() override;
-    void write(const std::string &x) override;
+    void write(const std::string &y) override;
     explicit SocketIO(int sock);
 };
 
